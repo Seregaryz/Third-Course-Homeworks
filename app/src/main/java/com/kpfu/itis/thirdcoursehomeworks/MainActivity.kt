1992ae6e5b4f9d2d1eb8384982a9a7d2c250a158
Sergey <seregaryz@gmail.com>
2020-10-18 23:25:50 +0300

    Add fourth homework - animations

--- app/src/main/java/com/kpfu/itis/thirdcoursehomeworks/MainActivity.kt
+++ app/src/main/java/com/kpfu/itis/thirdcoursehomeworks/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.kpfu.itis.thirdcoursehomeworks
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.AnimatorInflater
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var isBtnShowDown = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initOnClickListeners()
+    }
+
+    private fun initOnClickListeners(){
+        btnShow.setOnClickListener {
+            if(!isBtnShowDown) {
+                val upToDownAnimator =
+                    AnimatorInflater.loadAnimator(this, R.animator.up_to_down_animator)
+                upToDownAnimator.apply {
+                    setTarget(it)
+                    doOnStart {
+                        val appearanceAnimation = AnimationUtils.loadAnimation(this@MainActivity, R.anim.btn_appearance_to_visible)
+                        btnSpin.startAnimation(appearanceAnimation)
+                    }
+                    doOnEnd {
+                        btnSpin.visibility = View.VISIBLE
+                        isBtnShowDown = true
+                    }
+                    start()
+                }
+            } else {
+                val downToUpAnimator = AnimatorInflater.loadAnimator(this, R.animator.down_to_up_animator)
+                downToUpAnimator.apply {
+                    setTarget(it)
+                    doOnStart {
+                        val appearanceAnimation = AnimationUtils.loadAnimation(this@MainActivity, R.anim.btn_appearance_to_invisible)
+                        btnSpin.startAnimation(appearanceAnimation)
+                    }
+                    doOnEnd {
+                        btnSpin.visibility = View.INVISIBLE
+                        isBtnShowDown = false
+                    }
+                    start()
+                }
+            }
+        }
+        btnSpin.setOnClickListener {
+            val animationRotateCenter = AnimationUtils.loadAnimation(
+                this, R.anim.btn_rotation)
+            it.startAnimation(animationRotateCenter)
+        }
     }
 }
